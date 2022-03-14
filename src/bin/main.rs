2940006be0a5538a58a8fafd00b1eee64cd78a0b
Leonid Belyaev <l.belyaev@vk.team>
2022-03-15 01:21:06 +0300

    Threads = cores

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,11 @@ use highload_server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use num_cpus;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(num_cpus::get());
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
