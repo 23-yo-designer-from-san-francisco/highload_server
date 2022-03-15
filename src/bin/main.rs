0d00f6eabe367148e779662deed5f6c5cf34de68
Leonid Belyaev <l.belyaev@vk.team>
2022-03-15 21:16:22 +0300

    Status 405

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -33,66 +33,64 @@ fn handle_connection(mut stream: TcpStream) {
     }
 
     let request = String::from_utf8_lossy(&buffer);
-    // println!("{}", request);
 
-    let req = REQUEST_RE.captures(&request);//.len();//.get(0).map_or("", |m| m.as_str());
+    let req = REQUEST_RE.captures(&request);
 
     if let Some(matches) = req {
-        let req_type = matches.get(1).map_or("GET", |m| m.as_str());
-        let mut path = matches.get(2).map_or("/", |m| m.as_str()).to_string();
-        // println!("Req type: {}\nPath: {}", req_type, path);
-
-        // let (status_line, filename) = if buffer.starts_with(get) {
-        //     ("HTTP/1.1 200 OK", "index.html")
-        // } else {
-        //     ("HTTP/1.1 404 NOT FOUND", "404.html")
-        // };
-    
-        // let base_path = "/Users/l.belyaev/highload_server";
-        let base_path = env::var("SERVER_BASE_PATH").unwrap();
-        let mut full_path: String = base_path.to_owned();
-
-        let response: String;
-        if path == "/" || path.ends_with("/") {
-            path.push_str("index.html");
-        }
-        full_path.push_str(&path);
-        match fs::read(&full_path) {
-            Ok(contents) => {
-                let extension = Path::new(&full_path).extension().and_then(|s| s.to_str()).unwrap();
-                let content_type = match extension {
-                    "html" => "text/html",
-                    "css" => "text/css",
-                    "js" => "application/javascript",
-                    "jpg" => "image/jpeg",
-                    "jpeg" => "image/jpeg",
-                    "png" => "image/png",
-                    "gif" => "image/gif",
-                    "swf" => "application/x-shockwave-flash",
-                    _=>  "text/plain",
-                };
-
-                let status_line = "HTTP/1.1 200 OK";
-                response = format!(
-                    "{}\r\nDate: {}\r\nServer: {}\r\nContent-Length: {}\r\nConnection: {}\r\nContent-type: {}\r\n\r\n",
-                    status_line,
-                    "Today",
-                    "rust",
-                    contents.len(),
-                    "close",
-                    content_type,
-                );
-                stream.write(response.as_bytes()).unwrap();
-                stream.write(&contents).unwrap();
-            },
-            Err(_) => {
-                response = "HTTP/1.1 404 NOT FOUND".to_string();
-                stream.write(response.as_bytes()).unwrap();
-            }   
-        }
-    
-    
+        let method = matches.get(1).map_or("GET", |m| m.as_str());
+ 
+         match method {
+             "GET" | "HEAD" => {
+                let mut path = matches.get(2).map_or("/", |m| m.as_str()).to_string();
+                let base_path = env::var("SERVER_BASE_PATH").unwrap();
+                let mut full_path: String = base_path.to_owned();
         
+                let response: String;
+                if path == "/" || path.ends_with("/") {
+                    path.push_str("index.html");
+                }
+                full_path.push_str(&path);
+                match fs::read(&full_path) {
+                    Ok(contents) => {
+                        let extension = Path::new(&full_path).extension().and_then(|s| s.to_str()).unwrap();
+                        let content_type = match extension {
+                            "html" => "text/html",
+                            "css" => "text/css",
+                            "js" => "application/javascript",
+                            "jpg" => "image/jpeg",
+                            "jpeg" => "image/jpeg",
+                            "png" => "image/png",
+                            "gif" => "image/gif",
+                            "swf" => "application/x-shockwave-flash",
+                            _=>  "text/plain",
+                        };
+        
+                        let status_line = "HTTP/1.1 200 OK";
+                        response = format!(
+                            "{}\r\nDate: {}\r\nServer: {}\r\nContent-Length: {}\r\nConnection: {}\r\nContent-type: {}\r\n\r\n",
+                            status_line,
+                            "Today",
+                            "rust",
+                            contents.len(),
+                            "close",
+                            content_type,
+                        );
+                        stream.write(response.as_bytes()).unwrap();
+                        stream.write(&contents).unwrap();
+                    },
+                    Err(_) => {
+                        response = "HTTP/1.1 404 NOT FOUND".to_string();
+                        stream.write(response.as_bytes()).unwrap();
+                    }   
+                }
+             },
+             "POST"|"PUT"|"OPTIONS"|"DELETE"|"CONNECT"|"TRACE"|"PATCH" => {
+                let response = "HTTP/1.1 405 Method Not Allowed".to_string();
+                stream.write(response.as_bytes()).unwrap();
+             }
+             _ => {},
+         }
+    
         stream.flush().unwrap();
     } else {
         // println!("Parse error");
